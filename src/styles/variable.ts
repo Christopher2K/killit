557e25b8831b84da61c20c8724f16a4093c854b9
Christopher N. KATOYI KABA <christopher.katoyi@icloud.com>
2019-02-10 01:08:22 +0100

    feat: some variables..

---
+++ src/styles/variable.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+  dodgerBlue: '#2A7FFF',
+  linkWater: '#D5E0F2',
+  regentGray: '#8792A3',
+  paleSky: '#676F7C',
+  shuttleGray: '#525A69',
+  trout: '#505866',
+  tuna: '#2F343D'
+}
+
+export const Spaces = {
+  tiny: '1rem',
+  small: '2rem',
+  medium: '3rem',
+  large: '4rem',
+  largest: '5rem'
+}
